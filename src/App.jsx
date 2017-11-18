47626594590a7800b4132a2b61691fb797c5e4a6
Guilherme Monteiro <guilherme.mdasilva@gmail.com>
2017-11-18 18:38:00 -0400

    Fixes #3 - Added React-Bootstrap support

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import UserInformation from './UserInformation';
 
@@ -27,16 +26,13 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React Test</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.jsx</code> and save to reload.
-        </p>
         <div className="App-intro">
-          <hr />
           <p>Click on the button to fetch the user information</p>
-          <button onClick={this.getUserInformation()}>Click me</button>
+          <button type="button" className="btn btn-primary" onClick={this.getUserInformation()}>
+            Click me
+          </button>
         </div>
         <UserInformation />
       </div>
